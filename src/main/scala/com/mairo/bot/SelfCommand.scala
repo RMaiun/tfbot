9db243b4c60548fcbdbd11dfd174000da21dbc53
Roman Maiun <roman.maiun@unicorn.com>
2021-01-01 20:59:37 +0200

    improve /self cmd

--- src/main/scala/com/mairo/bot/SelfCommand.scala
+++ src/main/scala/com/mairo/bot/SelfCommand.scala
@@ -1,5 +1,7 @@
 package com.mairo.bot
 
+import java.time.{ZoneId, ZoneOffset, ZonedDateTime}
+
 import com.bot4s.telegram.models.Message
 import com.mairo.bot.ParentBot.notAvailable
 
@@ -12,5 +14,14 @@ trait SelfCommand {
        | userId = ${msg.from.fold(notAvailable)(x => x.id.toString)}
        | firstName = ${msg.from.fold(notAvailable)(x => x.firstName)}
        | lastName = ${msg.from.flatMap(x => x.lastName).fold(notAvailable)(x => x)}
-       | username = ${msg.from.flatMap(x => x.username).fold(notAvailable)(x => x)}""".stripMargin
+       | username = ${msg.from.flatMap(x => x.username).fold(notAvailable)(x => x)}
+       | serverH = ${ZonedDateTime.now().getHour}
+       | serverTS = ${ZonedDateTime.now().toString}
+       | serverUaH = ${ZonedDateTime.now(ZoneId.of("Europe/Kiev")).getHour}
+       | serverUaTS = ${ZonedDateTime.now(ZoneId.of("Europe/Kiev"))}""".stripMargin
+
+
+
+
+
 }
